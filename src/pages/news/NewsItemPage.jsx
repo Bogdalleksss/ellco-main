06842d0cbb842cc3e4316e1b1f2437bd3a6368cf
Богдан Викулин <vikubogdan@mail.ru>
2022-11-23 13:07:15 +0600

    -

--- src/pages/news/NewsItemPage.jsx
+++ src/pages/news/NewsItemPage.jsx
@@ -10,18 +10,27 @@ import Footer from "../../components/Footer/Footer";
 import { useDispatch, useSelector } from "react-redux";
 import { getImage } from "../../utils";
 import { newsFetchOne } from "../../store/news";
+import draftToHtml from 'draftjs-to-html';
 
 const NewsItemPage = () => {
   useScrollToTop()
   const dispatch = useDispatch();
   const formatDate = useFormatDate();
+  const [html, updateHtml] = useState();
   const post = useSelector(state => state.news.item);
   const news = useSelector(state => state.news.items);
   const { id } = useParams()
 
   useEffect(() => {
     dispatch(newsFetchOne(id));
-  }, [id])
+  }, [id]);
+
+  useEffect(() => {
+    if (post) {
+      const replaced = post.description.replace(/&quot;/gi, '"');
+      updateHtml(draftToHtml(JSON.parse(replaced)));
+    }
+  }, [post]);
 
   return (
     <>
@@ -34,7 +43,7 @@ const NewsItemPage = () => {
                 date={formatDate(post.createdAt)}
                 banner={getImage(post.image)}
               />
-              <NewsItemSection content={ { __html: post.description }} />
+              <NewsItemSection content={ { __html: html }} />
             </>
       }
       <NewsListSection className="news-other" news={news.slice(0, 4)}>
