6ba0935672e7bbba51ec2fefecb8ff4dad451a9d
Богдан Викулин <vikubogdan@mail.ru>
2022-12-01 23:48:10 +0600

    -

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,1 @@
-export const getImage = (uri) => uri ? `https://api.dev-huriky-1.site/storage/${uri}` : 'https://avatars.mds.yandex.net/get-autoru-vos/1894023/61931bfe5d45bd46d36e9a8ee8523170/1200x900n';
+export const getImage = (uri) => uri ? `https://api.ellco.ru/storage/${uri}` : 'https://avatars.mds.yandex.net/get-autoru-vos/1894023/61931bfe5d45bd46d36e9a8ee8523170/1200x900n';
