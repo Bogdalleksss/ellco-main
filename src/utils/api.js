db27ecf155cdd94f87dfc45ff60491ad8182730e
Богдан Викулин <vikubogdan@mail.ru>
2022-12-01 16:26:02 +0600

    -

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const $axios = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://api.dev-huriky-1.site/v1'
+  baseURL: process.env.REACT_APP_API_URL || 'https://api.ellco.ru/v1'
 });
